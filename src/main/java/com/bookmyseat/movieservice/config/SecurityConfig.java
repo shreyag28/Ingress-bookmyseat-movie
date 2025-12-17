5857192968208fdbabee4044df91dac77437744a
shwetkhai <shwetakhai@cybage.com>
2025-12-17 17:12:08 +0530

    updated pom

--- src/main/java/com/bookmyseat/movieservice/config/SecurityConfig.java
+++ src/main/java/com/bookmyseat/movieservice/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.bookmyseat.movieservice.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
